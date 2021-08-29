d64a78301c78ab383fa6fe2588aad08e5577e6a9
Abdulkareem Siddiq <github@siddiqsoft.com>
2021-08-29 02:02:35 -0700

    Updated tests to check for bad primary and rotateConnection on the Database object.

--- tests/CosmosClient.cpp
+++ tests/CosmosClient.cpp
@@ -77,3 +77,67 @@ TEST(CosmosClient, configure_1)
 	EXPECT_LE(1, currentConfig["serviceSettings"]["writableLocations"].size());
 	EXPECT_LE(1, cc.Database.currentConnection().WritableUris.size());
 }
+
+
+TEST(CosmosClient, discoverRegion)
+{
+	// These are pulled from Azure Pipelines mapped as secret variables into the following environment variables.
+	// WARNING!
+	// DO NOT DISPLAY the contents as they will expose the secrets in the Azure pipeline logs!
+	std::string priConnStr = std::getenv("CCTEST_PRIMARY_CS");
+	std::string secConnStr = std::getenv("CCTEST_SECONDARY_CS");
+
+	ASSERT_FALSE(priConnStr.empty())
+	        << "Missing environment variable CCTEST_PRIMARY_CS; Set it to Primary Connection string from Azure portal.";
+
+	siddiqsoft::CosmosClient cc;
+
+	EXPECT_NO_THROW(cc.configure({{"partitionKeyNames", {"__pk"}}, {"connectionStrings", {priConnStr, secConnStr}}}));
+
+	nlohmann::json info = cc;
+	EXPECT_EQ(2, info.size());
+
+	// Check that we have read/write locations detected.
+	// Atleast one read location
+	EXPECT_LE(1, cc.configure()["serviceSettings"]["readableLocations"].size());
+	EXPECT_LE(1, cc.Database.currentConnection().ReadableUris.size());
+	// Atleast one write location
+	EXPECT_LE(1, cc.configure()["serviceSettings"]["writableLocations"].size());
+	EXPECT_LE(1, cc.Database.currentConnection().WritableUris.size());
+}
+
+
+TEST(CosmosClient, discoverRegion_BadPrimary)
+{
+	// These are pulled from Azure Pipelines mapped as secret variables into the following environment variables.
+	// WARNING!
+	// DO NOT DISPLAY the contents as they will expose the secrets in the Azure pipeline logs!
+	std::string priConnStr = "AccountEndpoint=https://YOURDBNAME.documents.azure.com:443/"
+	                         ";AccountKey=U09NRUJBU0U2NEVOQ09ERURLRVlUSEFURU5EU1dJVEhTRU1JQ09MT04=;";
+	std::string secConnStr = std::getenv("CCTEST_SECONDARY_CS");
+
+	ASSERT_FALSE(priConnStr.empty())
+	        << "Missing environment variable CCTEST_PRIMARY_CS; Set it to Primary Connection string from Azure portal.";
+
+	siddiqsoft::CosmosClient cc;
+
+	EXPECT_NO_THROW(cc.configure({{"partitionKeyNames", {"__pk"}}, {"connectionStrings", {priConnStr, secConnStr}}}));
+
+	nlohmann::json info = cc;
+	EXPECT_EQ(2, info.size());
+
+	auto [rc, resp] = cc.discoverRegions();
+	// Expect failure.
+	EXPECT_EQ(12007, rc);
+
+	cc.Database.rotateConnection();
+	std::tie(rc, resp) = cc.discoverRegions();
+	// Expect success.
+	EXPECT_EQ(200, rc);
+
+	// Rotate..
+	cc.Database.rotateConnection();
+	// Try again.. we should fail again!
+	std::tie(rc, resp) = cc.discoverRegions();
+	EXPECT_EQ(12007, rc);
+}
