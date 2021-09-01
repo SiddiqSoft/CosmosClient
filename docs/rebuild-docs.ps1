2551e0a65cf25b0fb2fc2b5f8e49b8b0d84e8281
Abdulkareem Siddiq <github@siddiqsoft.com>
2021-08-31 22:16:34 -0700

    Fixed rebuild-docs.ps1

--- docs/rebuild-docs.ps1
+++ docs/rebuild-docs.ps1
@@ -1,3 +1,3 @@
-if( $Env:%GITVERSION_NUGETVERSION -eq "" ){ $ver = &{gitversion /output json /showvariable FullSemVer} }
-if( $Env:%GITVERSION_NUGETVERSION -ne "" ){ $ver = $Env:%GITVERSION_NUGETVERSION }
+if( $Env:GITVERSION_NUGETVERSION -eq "" ){ $ver = &{gitversion /output json /showvariable FullSemVer} }
+if( $Env:GITVERSION_NUGETVERSION -ne "" ){ $ver = $Env:GITVERSION_NUGETVERSION }
 &{ type ..\Doxyfile ; echo "PROJECT_NUMBER=$ver" } | doxygen -
