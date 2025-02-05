bfeaa1447275ba33a567b676e833c4870190b25f
Abdulkareem Siddiq <73565559+abdulkareem-siddiq@users.noreply.github.com>
2025-02-04 16:02:53 -0800

    Updated how we start docker.

--- tests/setup-emulator.sh
+++ tests/setup-emulator.sh
@@ -2,7 +2,8 @@ docker pull mcr.microsoft.com/cosmosdb/linux/azure-cosmos-emulator:vnext-preview
 docker run \
         --publish 8081:8081 \
         --publish 1234:1234 \
-        --name linux-emulator \
-        --replace \
+        --name=linux-emulator \
+        --rm \
+        --network=host \
         --detach \
         mcr.microsoft.com/cosmosdb/linux/azure-cosmos-emulator:vnext-preview
--- tests/setup-linux-emulator.bat
+++ tests/setup-linux-emulator.bat
@@ -1,2 +1,2 @@
 docker pull mcr.microsoft.com/cosmosdb/linux/azure-cosmos-emulator:latest
-docker run  --publish 8081:8081 --publish 10250-10255:10250-10255 --name linux-emulator --detach     mcr.microsoft.com/cosmosdb/linux/azure-cosmos-emulator:latest
+docker run  --publish 8081:8081 --publish 10250-10255:10250-10255 --name linux-emulator --detach  --network=host --rm  mcr.microsoft.com/cosmosdb/linux/azure-cosmos-emulator:latest
