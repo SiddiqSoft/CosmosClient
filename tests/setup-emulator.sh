24ba1fefca3c9f89a24175b1d08ae2b615d71983
Abdulkareem Siddiq <73565559+abdulkareem-siddiq@users.noreply.github.com>
2026-04-29 20:30:23 -0700

    fix: Updated the setup emulator for testing

--- tests/setup-emulator.sh
+++ tests/setup-emulator.sh
@@ -1,9 +1,9 @@
-docker pull mcr.microsoft.com/cosmosdb/linux/azure-cosmos-emulator:vnext-preview
+docker pull mcr.microsoft.com/cosmosdb/linux/azure-cosmos-emulator:vnext-EN20260331
 docker run \
         --publish 8081:8081 \
-        --publish 1234:1234 \
+        --publish 10250-10255:10250-10255 \
         --name=linux-emulator \
         --rm \
         --network=host \
         --detach \
-        mcr.microsoft.com/cosmosdb/linux/azure-cosmos-emulator:vnext-preview
+        mcr.microsoft.com/cosmosdb/linux/azure-cosmos-emulator:vnext-EN20260331
--- tests/setup-linux-emulator.bat
+++ tests/setup-linux-emulator.bat
@@ -1,2 +1,2 @@
-docker pull mcr.microsoft.com/cosmosdb/linux/azure-cosmos-emulator:latest
-docker run  --publish 8081:8081 --publish 10250-10255:10250-10255 --name linux-emulator --detach  --network=host --rm  mcr.microsoft.com/cosmosdb/linux/azure-cosmos-emulator:latest
+docker pull mcr.microsoft.com/cosmosdb/linux/azure-cosmos-emulator:vnext-EN20260331
+docker run  --publish 8081:8081 --publish 10250-10255:10250-10255 --name linux-emulator --detach  --network=host --rm  mcr.microsoft.com/cosmosdb/linux/azure-cosmos-emulator:vnext-EN20260331
