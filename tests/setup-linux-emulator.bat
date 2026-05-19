02d6425d26a18abb42ad158500921de50e78bebc
Abdulkareem Siddiq <73565559+abdulkareem-siddiq@users.noreply.github.com>
2026-05-19 07:00:01 -0700

    debug

--- tests/setup-linux-emulator.bat
+++ tests/setup-linux-emulator.bat
@@ -1,10 +1,15 @@
-REM Check if the linux-emulator container is already running
+@echo off
+echo "Check if the linux-emulator container is already running.."
 docker ps --filter "name=linux-emulator" --filter "status=running" --quiet | findstr . >nul
 if %errorlevel% equ 0 (
-    echo Docker container 'linux-emulator' is already running. Skipping start.
+    echo "Docker container 'linux-emulator' is already running. Skipping start."
     exit /b 0
 )
-
+echo "Pulling the latest Azure Cosmos DB Linux Emulator image..."
 docker pull mcr.microsoft.com/cosmosdb/linux/azure-cosmos-emulator:latest
+echo "Starting the Azure Cosmos DB Linux Emulator container..."
 docker run  --publish 8081:8081 --publish 10250-10255:10250-10255 --name linux-emulator --detach  --rm  mcr.microsoft.com/cosmosdb/linux/azure-cosmos-emulator:vnext-EN20260331
+echo "Waiting for the emulator to initialize..."
 timeout /t 30 /nobreak >nul
+
+@echo on
